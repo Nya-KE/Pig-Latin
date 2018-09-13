a1c5353e47dddb5b1b2b371effadc955a1f45307
Nyakio Mburu <nyakio.mburu@gmail.com>
2018-09-13 15:11:53 +0300

    adds translate vowel words ability

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//keeping mind 'cat' becoming 'atc' and then 'atcay'
+var userEntry = prompt("Enter word to translate:")
+
+if (userEntry.charAt(0) == "a" || userEntry.charAt(0) == "e" || userEntry.charAt(0) == "i" || userEntry.charAt(0) == "o" || userEntry.charAt(0) == "u" ) {
+  alert( userEntry + "ay")
+  } else {
+
+} 
+var reorgBox = []
+reorgBox.push(userEntry.split(""))
+.chart(0)
+alert(reorgBox)
